c2068e007ed767757fbdeb9f2dc278c9951b9852
Willian <10719@fai.com.br>
2021-07-08 03:40:32 -0300

    add bd-gravar.php

---
+++ back-end/user/bd-gravar.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once("connectDB.php");
+
+
+    $meu_BD = new BD();	
+	
+	$pdo = $meu_BD->pdo;
+
+
+    $sql = " insert into users	
+			(user, email, pass) 
+			values 
+			(:username,:email,:password) 
+			";
+
+    $cmd = $pdo->prepare($sql);
+
+        $username           = $_POST['username'];           
+		$email              = $_POST['email'];
+		$password           = $_POST['password'];
+
+
+		$cmd->bindValue(":username" , $username);                    
+		$cmd->bindValue(":email"    , $email); 
+		$cmd->bindValue(":password" , $password);
+
+		$cmd->execute();
+		header("Location: login.php");
+?>
