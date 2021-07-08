5f4796ea7631afdf077e98bc32f965bdbe251ae7
Renault Scabora <renaultscabora@hotmail.com>
2021-07-08 03:48:58 -0300

    remove sigin.php

--- back-end/user/bd-gravar.php
+++ back-end/user/bd-gravar.php
@@ -1,30 +1,29 @@
 <?php
 
-include_once("connectDB.php");
+	include_once("connectDB.php");
 
+	$meu_BD = new BD();	
 
-    $meu_BD = new BD();	
-	
 	$pdo = $meu_BD->pdo;
 
 
-    $sql = " insert into users	
+	$sql = " insert into users	
 			(user, email, pass) 
 			values 
-			(:username,:email,:password) 
-			";
+			(:username,:email,:password)";
 
-    $cmd = $pdo->prepare($sql);
+	$cmd = $pdo->prepare($sql);
 
-        $username           = $_POST['username'];           
-		$email              = $_POST['email'];
-		$password           = $_POST['password'];
+	$username           = $_POST['username'];           
+	$email              = $_POST['email'];
+	$password           = $_POST['password'];
 
 
-		$cmd->bindValue(":username" , $username);                    
-		$cmd->bindValue(":email"    , $email); 
-		$cmd->bindValue(":password" , $password);
+	$cmd->bindValue(":username" , $username);                    
+	$cmd->bindValue(":email"    , $email); 
+	$cmd->bindValue(":password" , $password);
 
-		$cmd->execute();
-		header("Location: login.php");
+	$cmd->execute();
+	header("Location: login.php");
+
 ?>
--- back-end/user/connectDB.php
+++ back-end/user/connectDB.php
@@ -6,20 +6,17 @@ $db = new mysqli(
     'vertrigo',
     'cloudnotes'
 );*/
-class BD
-{
+class BD {
 	public $pdo;
 
-	function __construct()
-	{
-		try 
-		{	
-			$this->pdo = new PDO("mysql:host=localhost;dbname=cloudnotes","root","vertrigo"); 
-		
-		} catch(PDOException $e)
-		{
+	function __construct() {
+
+		try {	
+			$this->pdo = new PDO("mysql:host=localhost;dbname=cloudnotes","root",""); 
+		} catch(PDOException $e) {
 			die('Não foi possível realizar a conexão com o Banco de Dados!!!');
-		}		
+		}
+
 	} // construct
 
 }
