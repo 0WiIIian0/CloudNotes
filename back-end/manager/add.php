0bfb2067e37870d97032157b35c5b43795203b7a
Willian <10719@fai.com.br>
2021-07-09 02:16:44 -0300

    change add.php, delete.php and edit.php

--- back-end/manager/add.php
+++ back-end/manager/add.php
@@ -1,4 +1,27 @@
-<?php
-    // test comment
-    echo "test";
+<?php    
+    session_start();
+    include_once("connectDB.php");
+
+    $meu_BD = new BD();	
+	
+	$pdo = $meu_BD->pdo;
+
+	$sql = " insert into notes	
+					(user,title,text) 
+				 values 
+					(:user,:title, :text) 
+				";
+
+		$cmd = $pdo->prepare($sql);
+
+		$user    = $_SESSION['id'];                    
+		$title   = $_POST['title'];     
+		$text    = $_POST['text'];
+
+
+		$cmd->bindValue(":user"    , $user);                    
+		$cmd->bindValue(":title"   , $title);         
+		$cmd->bindValue(":text"    , $text); 
+
+		$cmd->execute();
 ?>
