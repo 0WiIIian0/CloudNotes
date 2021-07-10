89964a02dc902686d60bc54fdbcb6f6ea59ca4f3
Renault Scabora <renaultscabora@hotmail.com>
2021-07-10 00:25:56 -0300

    Create base folders and add README files

--- back-end/manager/delete.php
+++ back-end/manager/delete.php
@@ -7,19 +7,19 @@
 	
 	$pdo = $my_DB->pdo;
 
-	$sql = "delete from notes where id = :id ";
+	$sql = "delete from notes where id = :id";
 
 	$cmd = $pdo->prepare($sql);
 
 	$cmd->bindValue(":id", $_SESSION['id'] );
 
-	if($dados = $cmd->fetch(PDO::FETCH_ASSOC))
+	if($cmd->execute())
 	{
 		echo 'Nota deletado com sucesso';
 	}
 	else
-   {
-   	 	echo 'Nao foi possivel deletar a nota';
-   }
+	{
+		echo 'Nao foi possivel deletar a nota';
+	}
 
 ?>
